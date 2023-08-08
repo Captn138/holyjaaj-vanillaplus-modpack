b2f8c31af98d5930ba925eb287e87df6b222ce62
Mickael Almeida <malmeida@et.esiea.fr>
2023-08-09 00:43:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-CPU_ARCH=$(lscpu | grep Architecture | awk '{print $2}')
+case "$(lscpu | grep Architecture | awk '{print $2}')" in
+aarch64)
+        CPU_ARCH=aarch64
+        ;;
+x86_64)
+        CPU_ARCH=x64
+        ;;
+esac
 MC_VERSION=1.20.1
 FABCIC_VERSION=0.14.22
 INSTALLER_VERSION=0.11.2
 FABRIC_URL=https://meta.fabricmc.net/v2/versions/loader/${MC_VERSION}/${FABCIC_VERSION}/${INSTALLER_VERSION}/server/jar
 JAVA_TAR=openjdk-17.0.2_linux-${CPU_ARCH}_bin.tar.gz
-JAVA_URL=https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/${JAVA_TAR}Us
+JAVA_URL=https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/${JAVA_TAR}
 JAVA_PATH=jre/jdk-17.0.2/bin/java
 JAR_NAME=fabric-server-mc.${MC_VERSION}-loader.${FABCIC_VERSION}-launcher.${INSTALLER_VERSION}.jar
 XMS=4G
@@ -15,7 +22,7 @@ XMX=12G
 if [ ! -f $JAVA_PATH ]
 then
         echo "Downloading OpenJDK 17..."
-        curl -sO $JAVA_URL
+        curl -s $JAVA_URL -o $JAVA_TAR
         if [ ! -d jre ]
         then
                 mkdir jre
