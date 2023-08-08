917502a45db110db45d4b8270a3983bc091475e1
Mickael Almeida <mickael.almeida1308@gmail.com>
2023-08-09 00:03:40 +0200

    Update start.sh to include amd64 CPUs

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+CPU_ARCH=$(lscpu | grep Architecture | awk '{print $2}')
 MC_VERSION=1.20.1
 FABCIC_VERSION=0.14.22
 INSTALLER_VERSION=0.11.2
-FABRIC_URL=https://meta.fabricmc.net/v2/versions/loader/$MC_VERSION/$FABCIC_VERSION/$INSTALLER_VERSION/server/jar
-JAVA_TAR=openjdk-17.0.2_linux-aarch64_bin.tar.gz
-JAVA_URL=https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/$JAVA_TAR
+FABRIC_URL=https://meta.fabricmc.net/v2/versions/loader/${MC_VERSION}/${FABCIC_VERSION}/${INSTALLER_VERSION}/server/jar
+JAVA_TAR=openjdk-17.0.2_linux-${CPU_ARCH}_bin.tar.gz
+JAVA_URL=https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/${JAVA_TAR}Us
 JAVA_PATH=jre/jdk-17.0.2/bin/java
-JAR_NAME=fabric-server-mc.$MC_VERSION-loader.$FABCIC_VERSION-launcher.$INSTALLER_VERSION.jar
+JAR_NAME=fabric-server-mc.${MC_VERSION}-loader.${FABCIC_VERSION}-launcher.${INSTALLER_VERSION}.jar
 XMS=4G
 XMX=12G
 
